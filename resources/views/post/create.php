4b641597ef8b165da1f3014b4c212a7b115d5e23
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-01-01 16:20:34 +0600

    first commit

---
+++ resources/views/post/create.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Create</title>
+</head>
+<body>
+    <h1>Create New Post</h1>
+    <form>
+        <input type="text" name="title" placeholder="Title Here"><br><br>
+        <textarea name="details" cols="30" rows="10" placeholder="Details"></textarea><br><br>
+        <button>Submit</button>
+    </form>
+</body>
+</html>
