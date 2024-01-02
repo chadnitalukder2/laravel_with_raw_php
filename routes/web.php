7b594da3b3efadef6eaece1743cfd35e379ff0c2
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-01-02 09:26:05 +0600

    Route Group Controller

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->name('post.')->group(function () {
+Route::controller(PostController::class)->name('post.')->group(function () {
 
-Route::get('posts',[PostController::class, 'index'])->name('index');
-Route::get('post/create',[PostController::class, 'create'])->name('create');
-Route::post('post/store',[PostController::class, 'store'])->name('store');
-Route::get('post/show/{id}',[PostController::class, 'show'])->name('show');
-Route::get('post/edit/{id}',[PostController::class, 'edit'])->name('edit');
-Route::get('post/update/{id}',[PostController::class, 'update'])->name('update');
-Route::delete('post/delete/{id}',[PostController::class, 'destroy'])->name('destroy');
+Route::get('posts', 'index')->name('index');
+Route::get('post/create','create')->name('create');
+Route::post('post/store','store')->name('store');
+Route::get('post/show/{id}', 'show')->name('show');
+Route::get('post/edit/{id}', 'edit')->name('edit');
+Route::get('post/update/{id}', 'update')->name('update');
+Route::delete('post/delete/{id}', 'destroy')->name('destroy');
 
 //Route::get('post/delete/{id}',[PostController::class, 'destroy']);  #worng proces
 });
